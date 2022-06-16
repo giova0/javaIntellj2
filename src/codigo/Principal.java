0844f98ec62562e1e1d44766c94abbcb390c40bb
giova0 <giova0@gmail.com>
2022-06-16 17:24:59 -0500

    first commit

---
+++ src/codigo/Principal.java
@@ -0,0 +1,24 @@
+package codigo;
+
+public class Principal {
+    public static void main(String[] args) {
+       //Definición de Variables
+        int variableEntera = 10;
+        System.out.println("Su valor es: " + variableEntera);
+        
+        String nombre = "Pedro";
+        System.out.println("Hola: " + nombre);
+        
+        byte varibleByte = 127;
+        System.out.println("valorByte: " + varibleByte);
+                
+        short variableShort = 30000;
+        System.out.println("variableShort: " + variableShort); 
+        
+        float variableFloat = 50.5f;
+        System.out.println("variableFloat: " + variableFloat); 
+        
+        double variableDouble = 30000;
+        System.out.println("variableDouble: " + variableDouble); 
+    }
+}
